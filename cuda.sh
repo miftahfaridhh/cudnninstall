5f3f9c1731f3a1ad8cc5473993058b6d6ef1331f
Muhammad Miftah Faridh <47894869+miftahfaridhh@users.noreply.github.com>
2023-05-20 00:59:28 +0900

    Add files via upload

---
+++ cuda.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+### steps ####
+# verify the system has a cuda-capable gpu
+# download and install the nvidia cuda toolkit and cudnn
+# setup environmental variables
+# verify the installation
+###
+
+### to verify your gpu is cuda enable check
+lspci | grep -i nvidia
+
+### If you have previous installation remove it first. 
+sudo apt-get purge nvidia*
+sudo apt remove nvidia-*
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt-get autoremove && sudo apt-get autoclean
+sudo rm -rf /usr/local/cuda*
+
+### Make GCC-10 as the default GCC version.
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y build-essential
+sudo apt install -y gcc-10 g++-10 cpp-10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+
+# system update
+sudo apt-get update
+sudo apt-get upgrade
+
+# install other import packages
+sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+
+# first get the PPA repository driver
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+
+# install nvidia driver with dependencies
+sudo apt install libnvidia-common-530
+sudo apt install libnvidia-gl-530
+sudo apt install nvidia-driver-530
+sudo apt install nvidia-dkms-530
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+#wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+
+
+sudo dpkg -i cudnn-local-repo-ubuntu2204-8.6.0.163_1.0-1_amd64.deb
+sudo cp /var/cudnn-local-repo-*/cudnn-local-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get install libcudnn8=8.6.0.163-1+cuda11.8
+sudo apt-get install libcudnn8-dev=8.6.0.163-1+cuda11.8
+sudo apt-get install libcudnn8-samples=8.6.0.163-1+cuda11.8
