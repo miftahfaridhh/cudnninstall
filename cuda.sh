7272d32f594f6783f774df15f75641dd46ff621c
Muhammad Miftah Faridh <47894869+miftahfaridhh@users.noreply.github.com>
2023-05-20 01:43:59 +0900

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -25,34 +25,37 @@ sudo apt install -y gcc-10 g++-10 cpp-10
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
 
 # system update
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # install other import packages
 sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
 
 # first get the PPA repository driver
 sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
+sudo apt update -y
 
 # install nvidia driver with dependencies
-sudo apt install libnvidia-common-530
-sudo apt install libnvidia-gl-530
-sudo apt install nvidia-driver-530
-sudo apt install nvidia-dkms-530
+sudo apt install libnvidia-common-530 -y
+sudo apt install libnvidia-gl-530 -y
+sudo apt install nvidia-driver-530 -y
+sudo apt install nvidia-dkms-530 -y
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install cuda
 
-
+wget https://developer.nvidia.com/compute/cudnn/secure/8.6.0/local_installers/11.8/cudnn-local-repo-ubuntu2204-8.6.0.163_1.0-1_amd64.deb
 sudo dpkg -i cudnn-local-repo-ubuntu2204-8.6.0.163_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-*/cudnn-local-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get install libcudnn8=8.6.0.163-1+cuda11.8
-sudo apt-get install libcudnn8-dev=8.6.0.163-1+cuda11.8
-sudo apt-get install libcudnn8-samples=8.6.0.163-1+cuda11.8
+sudo apt-get update -y
+sudo apt-get install libcudnn8=8.6.0.163-1+cuda11.8 -y
+sudo apt-get install libcudnn8-dev=8.6.0.163-1+cuda11.8 -y
+sudo apt-get install libcudnn8-samples=8.6.0.163-1+cuda11.8 -y
+
+nvidia-smi
+nvcc -V
